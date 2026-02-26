614a0494983ad44296a5658ffaf30e7df705861b
phucphan01866 <111278365+phucphan01866@users.noreply.github.com>
2026-02-27 00:11:38 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,11 +2,6 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 5432,
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'mydb',
-  max: 20
+  connectionString: process.env.DATABASE_URL,
 });
 module.exports = pool;
